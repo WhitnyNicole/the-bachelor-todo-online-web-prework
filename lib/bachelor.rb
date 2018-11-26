e03d9fe9bc4fc745d71feba1cfde1ffe925a5f5f
Whitny Edwards <wne4e2@gmail.com>
2018-11-26 00:54:10 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -18,16 +18,46 @@ def get_contestant_name(data, occupation)
         end 
       end 
     end 
+  end 
 end
 
 def count_contestants_by_hometown(data, hometown)
-  
+  counter = 0 
+    data.each do |season, array|
+      array.each do |hash|
+        hash.each do |k, v|
+          if v == hometown
+            counter += 1 
+          end 
+        end 
+      end 
+    end 
+  counter 
 end
 
 def get_occupation(data, hometown)
-  
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k, v|
+        if v == hometown
+          return hash["occupation"]
+        end 
+      end
+    end 
+  end 
 end
 
 def get_average_age_for_season(data, season)
-  
+  age = 0 
+  count = 0 
+    data[season].each do |hash|
+      hash.each do |k, v|
+        if k == "age"
+          count += 1 
+          age += v.to_f
+        end 
+      end 
+    end 
+    answer = (age/count).round 
+  return answer 
 end
