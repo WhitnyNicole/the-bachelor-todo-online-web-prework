81019f6a9ff0dd7a0aafef30f9b790712d6c010a
Whitny Edwards <wne4e2@gmail.com>
2018-11-26 00:49:28 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,9 @@
 def get_first_name_of_season_winner(data, season)
-  
+  data[season].each do |people|
+    people.each do |k, v|
+      if v == "Winner"
+        full_name = people["name"]
+        return full_name.split(" ").first
 end
 
 def get_contestant_name(data, occupation)
